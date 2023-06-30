178560459d0f60f11ee2e13b38b118c5811119b7
Ekuspreet <ekuspreetsingh2@gmail.com>
2023-07-01 02:53:12 +0530

    completed the visual representation of graphs using LeaderLine.js and also handles some anomalies

--- scripts/Graph.js
+++ scripts/Graph.js
@@ -1,8 +1,12 @@
+var value;
 let globalIndex = 1;
 var graph = [];
+var edges = [];
 var source, destination;
 var graphBox = document.querySelector("outerbox");
 var isSrc = false;
+var srcelem;
+var dstelem;
 
 //graph Functions
 
@@ -10,35 +14,54 @@ function insertNode(graph, src, dest, value) {
   graph.push({ src, dest, value });
 }
 
-// display letters on hover.
-
 graphBox.addEventListener("click", (event) => {
-  if (!isSrc) {
-    if (event.target.value == "") {
-      event.target.value = globalIndex;
-      source = globalIndex;
-      globalIndex++;
-    } else {
-      source = parseInt(event.target.value);
-    }
+  
+    if (!isSrc) {
+      srcelem = event.target;
+      if (event.target.value == "") {
+        event.target.value = globalIndex;
+        source = globalIndex;
+        globalIndex++;
+      } else {
+        source = parseInt(event.target.value);
+      }
 
-    isSrc = true;
-  } else if (isSrc) {
-    if (event.target.value == "") {
-      event.target.value = globalIndex;
-      destination = globalIndex;
-      globalIndex++;
-    } else {
-      destination = parseInt(event.target.value);
-    }
+      isSrc = true;
+    } else if (isSrc) {
+      dstelem = event.target;
 
-    
-     insertNode(
-      graph,
-      source,
-      destination,
-      parseInt(prompt(`Enter Value Of Edge From ${source} to ${destination}`))
-    );
-    isSrc = false;
-  }
+      if (event.target.value == "") {
+        event.target.value = globalIndex;
+        destination = globalIndex;
+        globalIndex++;
+      } else {
+        destination = parseInt(event.target.value);
+      }
+      value = parseInt(
+        prompt(`Enter Value Of Edge From ${source} to ${destination}`)
+      );
+      insertNode(graph, source, destination, value);
+
+      let lline = new LeaderLine(srcelem, dstelem, {
+        color: "red",
+        path: "straight",
+        size: '1',
+        middleLabel: LeaderLine.captionLabel(`${value}`, {
+          color: "black",
+          fontFamily: "poppins",
+        }),
+      });
+
+      edges.push(lline);
+      const index = graph.findIndex(
+        (obj) => obj.src === destination && obj.dest === source
+      );
+      if (index != -1) {
+        edges[index].path = "arc";
+        lline.path = "arc";
+      }
+
+      isSrc = false;
+    }
+  
 });
